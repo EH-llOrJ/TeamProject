13b6687e906cf153f13e8c40e35095c90b903a1b
kim-seung-hun <rlatmdgns16@naver.com>
2022-07-21 13:58:46 +0900

    20220721

--- MS/main.js
+++ MS/main.js
@@ -1,3 +1,5 @@
+let abc = true;
+
 //캔버스 변수 선언, 할당
 let canvasMain = document.getElementById("main");
 let ctxMain = canvasMain.getContext("2d");
@@ -41,6 +43,8 @@ let player = {
         ? runPlayer[this.index]
         : this.state == "slide"
         ? slidePlayer[this.index]
+        : this.state == "jumpstart"
+        ? jumpPlayer[this.index]
         : this.state == "jump"
         ? jumpPlayer[this.index]
         : this.state == "dbjumpstart"
@@ -49,8 +53,12 @@ let player = {
         ? dbjumpPlayer[this.index]
         : this.state == "dbjumplast"
         ? dbjumplastPlayer[this.index]
-        : this.state == "fallen"
-        ? fallenPlayer[this.index]
+        : this.state == "falling"
+        ? fallingPlayer[this.index]
+        : this.state == "coll"
+        ? collPlayer[this.index]
+        : this.state == "end"
+        ? runPlayer[this.index]
         : null,
       this.x,
       this.y,
@@ -63,13 +71,53 @@ let player = {
     this.y += this.yspeed;
     this.yspeed += gravity;
 
-    //땅에 붙으면 하락값 0
+    //바닥에 캐릭터 닿으면 순간 yspeed를 0으로 만들어서 띄움
     for (let i = 0; i < floor.length; i++)
-      if (this.y + this.height > floor[i].height) {
+      // hi();
+      if (
+        this.y + this.height > canvasMain.height + 100 &&
+        player.state == "run"
+      ) {
+        player.y = player.y - 0.1;
+        player.state = "jump";
+        jump = true;
+        floatPlayer = true;
+        jumpTimer = 0;
+        falling = true;
+        player.state = "falling";
+        this.yspeed = -7;
+      } else if (this.y + this.height > canvasMain.height + 100) {
+        jumpTimer = 0;
+        falling = true;
+        player.state = "falling";
+        this.yspeed = -7;
+      } else {
         this.yspeed += gravity;
-      } else this.yspeed = 0;
+        falling = false;
+      }
+    hi();
+    if (player.state == "end") {
+      player.state = "end";
+    }
   },
 };
+function hi() {
+  if (player.y < 230 && player.state == "falling" && abc == true) {
+    abc = false;
+    let fallingfin = setInterval(() => {
+      abc = false;
+      gravity = 0;
+      player.y = 230;
+    }, 10);
+    setTimeout(() => {
+      player.state = "dbjumplast";
+      clearInterval(fallingfin);
+      yspeed = 0;
+      gravity = 0.009;
+      abc = true;
+    }, 500);
+  }
+}
 
 //플레이어 이미지 프레임변경
 //달리기 이미지
@@ -143,12 +191,38 @@ for (let i = 0; i < 4; i++) {
   dbjumplastPlayer.push(new Image());
   dbjumplastPlayer[i].src = imglinkDbjumplast[i];
 }
+let fallingPlayer = new Array();
+let imglinkfallingPlayer = [
+  "images/Character/Taehoon/Fall/Fall1.png",
+  "images/Character/Taehoon/Fall/Fall2.png",
+  "images/Character/Taehoon/Fall/Fall1.png",
+  "images/Character/Taehoon/Fall/Fall2.png",
+];
+for (let i = 0; i < 4; i++) {
+  fallingPlayer.push(new Image());
+  fallingPlayer[i].src = imglinkfallingPlayer[i];
+}
 
 //피격시 이미지
+let collPlayer = new Array();
+let imglinkcollPlayer = [
+  "images/Character/Taehoon/Collision/Collision1.png",
+  "images/Character/Taehoon/Collision/Collision2.png",
+  "images/Character/Taehoon/Collision/Collision1.png",
+  "images/Character/Taehoon/Collision/Collision2.png",
+];
+for (let i = 0; i < 4; i++) {
+  collPlayer.push(new Image());
+  collPlayer[i].src = imglinkcollPlayer[i];
+}
 
 //점프기능
 function jumpSkill() {
   //점프시 점프값 증가 & 이미지 변경
+  if (player.state == "jumpstart" && jumpTimer > 30) {
+    player.state = "jump";
+  }
+
   if (jump == true) {
     player.y -= 7.57;
     jumpTimer++;
@@ -157,6 +231,11 @@ function jumpSkill() {
   //더블점프
   if (dbjump == true) {
     player.y -= 0.45;
+    jumpTimer++;
+  }
+
+  if (falling == true) {
+    player.y -= 7.57;
     jumpTimer++;
   }
 
@@ -171,6 +250,122 @@ function jumpSkill() {
   if (player.state == "dbjump" && jumpTimer > 10) {
     player.y -= 0;
   }
+  if (player.state == "falling" && jumpTimer > 100) {
+    let fallingcome = setInterval(() => {
+      player.y -= 0;
+    }, 1);
+    setTimeout(() => {
+      clearInterval(fallingcome);
+    }, 3000);
+  }
+}
+
+//허들 충돌 및 피격
+function TopHurdle() {
+  //앉는 허들
+  for (let i = 0; i < hurdle.length; i++) {
+    col_left_X = hurdle[i].x - player.x;
+    col_right_X = hurdle[i].x + hurdle[i].width - (player.x + player.width);
+    if (
+      col_left_X < 85 &&
+      col_left_X > -100 &&
+      (col_right_X < 100) & (col_right_X > -30) &&
+      player.state != "slide" &&
+      a == true
+    ) {
+      col_temp--;
+      console.log(col_temp);
+      console.log("충돌");
+      player.state = "coll";
+      a = false;
+      let hi = setInterval(() => {
+        a = false;
+        player.state = "coll";
+      }, 1);
+      setTimeout(() => {
+        clearInterval(hi);
+        player.state = "run";
+        a = true;
+      }, 1000);
+    }
+  }
+}
+
+function LowHurdle() {
+  //Low 허들
+  for (let i = 0; i < lowhurdle.length; i++) {
+    col_left_X = lowhurdle[i].x - player.x;
+    col_right_X =
+      lowhurdle[i].x + lowhurdle[i].width - (player.x + player.width);
+    col_top_Y = lowhurdle[i].y - player.y;
+    col_bottom_Y =
+      lowhurdle[i].y + lowhurdle[i].height - (player.y + player.height);
+    if (
+      col_left_X > -50 &&
+      col_left_X < 50 &&
+      col_right_X < 50 &&
+      col_right_X > -50 &&
+      col_top_Y < 55 &&
+      col_top_Y > -55 &&
+      col_bottom_Y > -55 &&
+      col_bottom_Y < 55 &&
+      a == true
+    ) {
+      col_temp--;
+      console.log(col_temp);
+      console.log("충돌");
+      player.state = "coll";
+      a = false;
+      let hi = setInterval(() => {
+        a = false;
+        player.state = "coll";
+      }, 1);
+      setTimeout(() => {
+        clearInterval(hi);
+        a = true;
+        player.state = "run";
+      }, 1000);
+    }
+  }
+}
+
+function MiddleHurdle() {
+  //high 허들
+  for (let i = 0; i < highhurdle.length; i++) {
+    col_left_X = highhurdle[i].x - player.x;
+    col_right_X =
+      highhurdle[i].x + highhurdle[i].width - (player.x + player.width);
+    col_top_Y = highhurdle[i].y - player.y;
+    col_bottom_Y =
+      highhurdle[i].y + highhurdle[i].height - (player.y + player.height);
+
+    if (
+      col_left_X > -70 &&
+      col_left_X < 70 &&
+      col_right_X > -70 &&
+      col_right_X < 70 &&
+      col_top_Y < 90 &&
+      col_top_Y > -90 &&
+      col_bottom_Y > -90 &&
+      col_bottom_Y < 90 &&
+      a == true
+    ) {
+      col_temp--;
+      console.log(col_temp);
+      console.log("충돌");
+      player.state = "coll";
+      a = false;
+      let hi = setInterval(() => {
+        a = false;
+        player.state = "coll";
+      }, 1);
+      setTimeout(() => {
+        clearInterval(hi);
+        a = true;
+        player.state = "run";
+      }, 1000);
+    }
+  }
 }
 
 //전역변수(frame=프레임, jumpTimer = 점프시간)
@@ -178,14 +373,17 @@ let frame = 0;
 let jumpTimer = 0;
 let jump = false;
 let dbjump = false;
-
-//키 코드 확인3
-// addEventListener("keydown", function () {
-//   console.log(this.event);
-// });
-
+let falling = false;
 let isSliding = false;
 let floatPlayer = false;
+
+// 시작 멈춤 버튼 상태
+// playBtn.addEventListener("click", function () {
+//   continueAnimating = true;
+// });
+// pauseBtn.addEventListener("click", function () {
+//   continueAnimating = false;
+// });
 
 //키 이벤트
 document.addEventListener("keydown", function (key) {
@@ -197,16 +395,19 @@ document.addEventListener("keydown", function (key) {
           player.state = "run";
           player.height = 90;
           player.width = 80;
-          player.state = "jump";
+          player.state = "jumpstart";
         }
         player.y = player.y - 0.1;
-        player.state = "jump";
+        player.state = "jumpstart";
         jump = true;
         floatPlayer = true;
         break;
     }
   }
-  if (player.state == "jump" && jumpTimer > 10) {
+  if (
+    (player.state == "jumpstart" || player.state == "jump") &&
+    jumpTimer > 10
+  ) {
     switch (key.code) {
       case "Space":
         jumpTimer = 0;
@@ -218,14 +419,6 @@ document.addEventListener("keydown", function (key) {
   }
 
   switch (key.code) {
-    case "KeyA":
-      player.x -= 10;
-      break;
-
-    case "KeyD":
-      player.x += 10;
-      break;
-
     case "ArrowDown":
       if (
         player.state == "jump" ||
@@ -264,8 +457,24 @@ document.addEventListener("keyup", function (key) {
   }
 });
 
+let a = true;
+let b = 0;
+let col_temp = 100;
+let continueAnimating = true;
+
 //게임실행
 function game() {
+  //멈춤 버튼 클릭시 애니메이션 멈춤 & hp 모두 소모시 정지 & 클리어젤리 먹을시
+  if (HpDecrease.x <= 34 || !continueAnimating || player.state == "end") {
+    cancelAnimationFrame(game);
+    cancelAnimationFrame(game);
+    cancelAnimationFrame(game);
+    cancelAnimationFrame(game);
+    cancelAnimationFrame(game);
+  } else {
+    requestAnimationFrame(game);
+  }
+
   frame++;
 
   //전체 영역 클리어
@@ -273,26 +482,35 @@ function game() {
   ctxBackground.clearRect(0, 0, canvasBackground.width, canvasBackground.width);
 
   //땅 올라타기
+  //밑에 땅
   for (let i = 0; i < floor.length; i++) {
     if (
       player.y + player.height >= floor[i].y &&
       player.x + player.width - 10 >= floor[i].x &&
-      player.x + 30 <= floor[i].x + floor[i].width
+      player.x + 30 <= floor[i].x + floor[i].width &&
+      player.state != "falling"
     ) {
       player.yspeed = 0;
       floatPlayer = false;
       jumpTimer = 0;
       jump = false;
       dbjump = false;
-      if (player.state != "slide") {
+      if (player.state == "coll") {
+        player.state = "coll";
+      } else if (player.state != "slide") {
         player.state = "run";
         player.y = 420;
       }
     }
   }
-
-  if (floatPlayer == true) {
-    for (let i = 0; i < floatFloor.length; i++) {
+  //위에 땅
+  for (let i = 0; i < floatFloor.length; i++) {
+    if (
+      player.y + 50 <= floatFloor[i].y &&
+      player.state != "dbjumpstart" &&
+      player.state != "jumpstart" &&
+      player.state != "falling"
+    ) {
       if (
         player.y + player.height >= floatFloor[i].y &&
         player.x + player.width - 10 >= floatFloor[i].x &&
@@ -303,10 +521,32 @@ function game() {
         jumpTimer = 0;
         jump = false;
         dbjump = false;
-        if (player.state != "slide") {
+        if (player.state == "coll") {
+          player.state = "coll";
+        } else if (player.state != "slide") {
           player.state = "run";
         }
       }
+    }
+  }
+
+  //충돌시
+  for (let i = 0; i < hurdle.length; i++) {
+    if (
+      hurdle[i].x < 200 &&
+      hurdle[i].x > 0 &&
+      player.height == 90 &&
+      a == true
+    ) {
+      col_temp--;
+      a = false;
+      let hi = setInterval(() => {
+        a = false;
+      }, 1);
+      setTimeout(() => {
+        clearInterval(hi);
+        a = true;
+      }, 1000);
     }
   }
 
@@ -324,7 +564,12 @@ function game() {
   hurdle.forEach((hurdle) => {
     hurdle.draw();
   });
-
+  lowhurdle.forEach((lowhurdle) => {
+    lowhurdle.draw();
+  });
+  highhurdle.forEach((highhurdle) => {
+    highhurdle.draw();
+  });
   player.update();
 
   //hp.js함수 실행
@@ -332,11 +577,12 @@ function game() {
   jamDraw();
   HpDecrease.draw();
   HpLight.draw();
+  TopHurdle();
+  LowHurdle();
+  MiddleHurdle();
 
-  if (HpDecrease.x <= 30) {
-    cancelAnimationFrame(game);
-  } else {
-    requestAnimationFrame(game);
+  if (floor[26].x < 0) {
+    floor[26].x = 0;
   }
 }
 //실행
@@ -348,3 +594,4 @@ document.getElementById("tutorial").onclick = function () {
   game();
   document.getElementById("tutorial").style.zIndex = "-1";
 };
+game();
